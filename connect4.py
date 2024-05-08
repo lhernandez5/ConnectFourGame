8d0624311dfa410e3c953d659e07fde5e2e1b36f
Luz Hernandez <hernandezluz18@gmail.com>
2024-05-07 22:14:35 -0400

    updated logic

--- connect4.py
+++ connect4.py
@@ -2,16 +2,18 @@ import numpy as np
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
+game_over = False
+turn = 0
 
 def create_board():
-    board= np.zeros((6,7))
+    board= np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
 def is_valid_space(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
@@ -24,10 +26,28 @@ def print_board(board):
 def winning_move(board, piece):
     return 0
 
+def winning_move(board, piece):
+    for r in range(ROW_COUNT):
+        for c in range(COLUMN_COUNT):
+            if c <= COLUMN_COUNT - 4:
+                # Horizontal wins
+                if all(board[r][c+i] == piece for i in range(4)):
+                    return True
+                
+                # Positive diagonal wins
+                if r <= ROW_COUNT - 4 and all(board[r+i][c+i] == piece for i in range(4)):
+                    return True
+                
+            # Vertical wins
+            if r <= ROW_COUNT - 4 and all(board[r+i][c] == piece for i in range(4)):
+                return True
+                
+            # Negative diagonal wins
+            if (c <= COLUMN_COUNT - 4 and r >= 3) and all(board[r-i][c+i] == piece for i in range(4)):
+                return True
+            
 board = create_board()
 print_board(board)
-game_over = False
-turn = 0
 
 while not game_over:
     #Ask for Player 1 input
@@ -37,6 +57,10 @@ while not game_over:
         if is_valid_space(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print("PLAYER 1 WINS!!! CONGRATS!!!")
+                game_over = True
     #Ask for Player 2 input
     else:
         col = int(input("Player 2 make your selection (0-6):"))
@@ -44,6 +68,10 @@ while not game_over:
         if is_valid_space(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+            if winning_move(board, 2):
+                print("PLAYER 2 WINS!!! CONGRATS!!!")
+                game_over = True
+
     print_board(board)
         
     turn += 1
